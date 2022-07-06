9585985a8a7ebf747fb80bdcbff2c427b68194f6
Renan <renan.lzgil@gmail.com>
2022-07-06 21:43:27 +0200

    linter update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
+  mode: "jit",
+  purge: [
+    "./src/pages/**/*.tsx",
+    "./src/components/**/*.tsx",
+    "./src/features/**/*.tsx",
+  ],
   darkMode: false,
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {},
   },
